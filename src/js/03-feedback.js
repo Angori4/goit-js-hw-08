38f70192c745b4a17def8baef131fdfb4f8a6b7b
Angori4 <pavlichuk0810@gmail.com>
2023-01-04 21:27:06 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,43 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector(".feedback-form");
-const message= document.querySelector(".feedback-form textarea");
-const email = document.querySelector(".feedback-form input")
-const STORAGE_KEY = "feedback-form-state";
+const LOCAL = 'selectedFilters';
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
 
 let formData = {};
 
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onFormInput, 500));
+onPageLoad();
 
-function onFormInput(event) {
-localStorage.setItem(STORAGE_KEY, JSON.stringify({ email: email.value, message: message.value }));
-}
-
-function onFormSubmit(event) {
-    event.preventDefault();
-    console.log({ email: email.value, message: message.value });
-    localStorage.removeItem(STORAGE_KEY);
-    event.currentTarget.reset();
-}
-
-try {
-  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (formData !== null) {
-    email.value = formData.email;
-    message.value = formData.message;
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const email = evt.target.elements.email.value;
+  const message = evt.target.elements.message.value;
+  if (email === '' || message === '') {
+    alert ("fill all the fields")
+  } else {
+    console.dir(JSON.parse(localStorage.getItem(LOCAL)));
+    localStorage.removeItem(LOCAL);
+    formData = {};  
   }
-}catch (error) {
-  console.log(error);
+  
+  evt.currentTarget.reset();  
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCAL, JSON.stringify(formData));
+}
+ 
+
+function onPageLoad() {
+  const savedData = JSON.parse(localStorage.getItem(LOCAL));
+
+  if (savedData) {
+    Object.entries(savedData).forEach(([key, value]) => {
+      formEl[key].value = value;
+      formData[key] = value;
+    });
+  }
 }
